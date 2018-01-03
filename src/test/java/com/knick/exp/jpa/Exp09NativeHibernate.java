ba52062869b6ed2fd6373d8def9712a27ca0ea9d
Kiryl Nikalayenka <knikalayenka@fitbit.com>
2018-01-03 13:22:14 +0300

    Played with multiple transactions

--- src/test/java/com/knick/exp/jpa/Exp09NativeHibernate.java
+++ src/test/java/com/knick/exp/jpa/Exp09NativeHibernate.java
@@ -41,6 +41,27 @@ public class Exp09NativeHibernate {
         Assert.assertEquals(TransactionStatus.COMMITTED, session.getTransaction().getStatus());
     }
 
+    @Test
+    public void playWithMultipleTransactions() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("MYSQL_PU");
+        SessionFactory sf = emf.unwrap(SessionFactory.class);
+        Session session = sf.openSession();
+        Assert.assertEquals(TransactionStatus.NOT_ACTIVE, session.getTransaction().getStatus());
+        session.beginTransaction();
+        Assert.assertEquals(TransactionStatus.ACTIVE, session.getTransaction().getStatus());
+        Message msg = Message.builder().text("message").build();
+        session.save(msg);
+        session.getTransaction().rollback();
+        Assert.assertEquals(TransactionStatus.ROLLED_BACK, session.getTransaction().getStatus());
+
+        session.beginTransaction();
+        Assert.assertEquals(TransactionStatus.ACTIVE, session.getTransaction().getStatus());
+        msg = Message.builder().text("message1").build();
+        session.save(msg);
+        session.getTransaction().commit();
+        Assert.assertEquals(TransactionStatus.COMMITTED, session.getTransaction().getStatus());
+    }
+
     private static class TestInterceptor extends EmptyInterceptor {
         @Override
         public void postFlush(Iterator entities) {
